0512bb8342d11d755144609efc60961694d00fbb
Kenny D. Lee <kennyd.lee@outlook.com>
2015-09-25 18:49:35 -0400

    Update Canvas_Shapes.c

--- Canvas_Shapes.c
+++ Canvas_Shapes.c
@@ -1,11 +1,10 @@
-//hit from the 3DSPong
-//
-//
-//
-//
 #include "Canvas_Shapes.h"
 
-//Revised from --> http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C
+//with exception of poly && poly3 && unless stated/commented in the above otherwise source were cited -->
+//https://github.com/shinyquagsire23/DCPU-3DS
+//https://github.com/AlbertoSONIC/3DS_Paint
+
+//Revised --> http://rosettacode.org/wiki/Bitmap/Bresenham%27s_line_algorithm#C
 void line(u8* screen, int x0, int y0, int x1, int y1, u32 colour) {
   int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
   int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
@@ -20,26 +19,36 @@ void line(u8* screen, int x0, int y0, int x1, int y1, u32 colour) {
   }
 }
 
-void LineHV(u8* screen, int x1, int y1, int x2, int y2, u32 colour)
+//-->
+void poly(u8* screen, int x1, int y1, int x2, int y2, u32 colour)
 {
-	int x, y;
-	if (x1 == x2){
-		if (y1<y2) for (y = y1; y < y2; y++) SetCanvasPixel(screen,x1,y,colour);
-		else for (y = y2; y < y1; y++) SetCanvasPixel(screen,x1,y,colour);
-	} else {
-		if (x1<x2) for (x = x1; x < x2; x++) SetCanvasPixel(screen,x,y1,colour);
-		else for (x = x2; x < x1; x++) SetCanvasPixel(screen,x,y1,colour);
+	line(screen, x1, y1, x2, y1, colour);
+	line(screen, x2, y1, x2, y2, colour);
+	line(screen, x1, y2, x2, y2, colour);
+	line(screen, x1, y1, x1, y2, colour);
+}
+
+//-->
+void poly3(u8* screen, int x1, int y1, int x2, int y2, int x3, int y3, u32 colour)
+{
+	line(screen, x1, y1, x2, y2, colour);
+	line(screen, x2, y2, x3, y3, colour);
+	line(screen, x3, y3, x1, y1, colour);
+}
+
+//altered --> https://bitbucket.org/xerpi/eleven-arms/src/
+void rectfill(u8* screen, int x, int y, int w, int h, u32 colour)
+{
+	int i, j;
+	for (i = 0; i < w; ++i) {
+		for (j = 0; j < h; ++j) {
+			SetCanvasPixel(screen, x+i, y+j, color);
+		}
 	}
 }
 
-void box(u8* screen, int x1, int y1, int x2, int y2, u32 colour)
-{
-	drawLine(screen, x1, y1, x2, y1, colour);
-	drawLine(screen, x2, y1, x2, y2, colour);
-	drawLine(screen, x1, y2, x2, y2, colour);
-	drawLine(screen, x1, y1, x1, y2, colour);
-}
-
+/*
+#define Circle(screen, cx, cy, radius, colour) (Ellipse(screen, cx, cy, radius, radius, colour));
 void Circle(u8* screen, int xCen, int yCen, int radius, u32 colour)
 {
 	int x = 0;
@@ -58,6 +67,7 @@ void Circle(u8* screen, int xCen, int yCen, int radius, u32 colour)
 		Circum(screen, xCen, yCen, x, y, colour);
 	}
 }
+*/
 
 void CircleFill(u8* screen, int xCen, int yCen, int radius, u32 colour)
 {
@@ -70,6 +80,7 @@ void CircleFill(u8* screen, int xCen, int yCen, int radius, u32 colour)
 	}
 }
 
+/*
 void Circum(u8* screen, int cx, int cy, int x, int y, u32 colour)
 {
 
@@ -96,9 +107,10 @@ void Circum(u8* screen, int cx, int cy, int x, int y, u32 colour)
 		SetCanvasPixel(screen, cx - y, cy - x, colour);
 	}
 }
+*/ 
 
-
-//Revised from -->http://www.programming-techniques.com/2012/01/drawing-ellipse-with-mid-point-ellipse.html
+//ellipsePlotPoints && Ellipse && ROUND
+//Revised --> http://www.programming-techniques.com/2012/01/drawing-ellipse-with-mid-point-ellipse.html
 #define ROUND(a) ((int) (a + 0.5))
 
 void ellipsePlotPoints(u8* screen, int xCenter, int yCenter, int x, int y, u32 colour){
@@ -149,4 +161,5 @@ void Ellipse(u8* screen, int xCenter, int yCenter, int Rx, int Ry, u32 colour){
         }
         ellipsePlotPoints(screen, xCenter, yCenter, x, y, colour);
     }
+
 }
