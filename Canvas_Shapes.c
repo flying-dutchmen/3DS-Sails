4797123cc28b61e9e6b8875bdcad9b4f1b4221dc
Kenny D. Lee <kennyd.lee@outlook.com>
2015-09-29 21:26:36 -0400

    todo: CircleFill will be deprecated for floodfill
    removed all old code for Ellipse & circle is over-hall

--- Canvas_Shapes.c
+++ Canvas_Shapes.c
@@ -1,6 +1,6 @@
 #include "Canvas_Shapes.h"
 
-//with exception of poly && unless stated/commented in the above otherwise source were cited -->
+//with exception of poly && Ellipse && unless stated/commented in the above otherwise source were cited elsewear-->
 //https://github.com/shinyquagsire23/DCPU-3DS
 //https://github.com/AlbertoSONIC/3DS_Paint
 
@@ -19,6 +19,7 @@ void line(u8* screen, int x0, int y0, int x1, int y1, u32 colour) {
   }
 }
 
+//tested &or finaldraft
 void box(u8* screen, int x, int y, int w, int h, u32 colour)
 {
 	line(screen, x, y, x+w, y, colour); //top
@@ -27,16 +28,18 @@ void box(u8* screen, int x, int y, int w, int h, u32 colour)
 	line(screen, x+w, y, x+w, y+h, colour); //right
 }
 
-//plot nth number vertex -->  tested &or draftfinal
+//plot nth number vertex -->  tested &or finaldraft
 void poly(u8* screen, int ** argv, int argc, u32 colour)
 //void poly(u8* screen, int * argv, int argc, u32 colour)
 //void poly(u8* screen, int *argv[], int argc, u32 colour)
-{ 
+//in the begining there was void poly(u8* screen, int x1, int y1, int x2, int y2, int x3, int y3, u32 colour)
+{
     int i;
-    for(i =0;i<argc-2;i+=2) //roll through all vertices && increment 2 positions
-      	line(screen, argv[i], argv[i+1], argv[i+2], argv[i+3], colour);
-//Close polygon      	
-      	line(screen, argv[0], argv[1], argv[argc-1], argv[argc], colour);
+//roll through the array
+    for(i=0;i<argc-2;i+=2)
+      	line(screen, argv[i], argv[i+1], argv[i+2], argv[i+3], colour); 
+//automatically Close polygon      	
+        line(screen, argv[0], argv[1], argv[argc-1], argv[argc], colour);
 }
 
 //altered --> https://bitbucket.org/xerpi
@@ -50,28 +53,8 @@ void rectfill(u8* screen, int x, int y, int w, int h, u32 colour)
 	}
 }
 
-/*
-#define Circle(screen, cx, cy, radius, colour) (Ellipse(screen, cx, cy, radius, radius, colour));
-void Circle(u8* screen, int xCen, int yCen, int radius, u32 colour)
-{
-	int x = 0;
-	int y = radius;
-	int p = (5 - radius*4)/4;
-	Circum(screen, xCen, yCen, x, y, colour);
-	while(x < y){
-		x++;
-		if(p < 0){
-			p += 2*x+1;
-		}
-		else{
-			y--;
-			p += 2*(x-y)+1;
-		}
-		Circum(screen, xCen, yCen, x, y, colour);
-	}
-}
-*/
-
+//#define Circle(screen, cx, cy, radius, colour) (Ellipse(screen, cx, cy, radius, radius, 0, colour));
+//not at all happy with LCD output, too be nixxed &or deprecated for floodfill
 void CircleFill(u8* screen, int xCen, int yCen, int radius, u32 colour)
 {
 	Circle(screen, xCen, yCen, radius, colour);
@@ -83,93 +66,8 @@ void CircleFill(u8* screen, int xCen, int yCen, int radius, u32 colour)
 	}
 }
 
-/*
-void Circum(u8* screen, int cx, int cy, int x, int y, u32 colour)
-{
-
-	if(x == 0){
-		SetCanvasPixel(screen, cx, cy + y, colour);
-		SetCanvasPixel(screen, cx, cy - y, colour);
-		SetCanvasPixel(screen, cx + y, cy, colour);
-		SetCanvasPixel(screen, cx - y, cy, colour);
-	}
-	if(x == y){
-		SetCanvasPixel(screen, cx + x, cy + y, colour);
-		SetCanvasPixel(screen, cx - x, cy + y, colour);
-		SetCanvasPixel(screen, cx + x, cy - y, colour);
-		SetCanvasPixel(screen, cx - x, cy - y, colour);
-	}
-	if(x < y){
-		SetCanvasPixel(screen, cx + x, cy + y, colour);
-		SetCanvasPixel(screen, cx - x, cy + y, colour);
-		SetCanvasPixel(screen, cx + x, cy - y, colour);
-		SetCanvasPixel(screen, cx - x, cy - y, colour);
-		SetCanvasPixel(screen, cx + y, cy + x, colour);
-		SetCanvasPixel(screen, cx - y, cy + x, colour);
-		SetCanvasPixel(screen, cx + y, cy - x, colour);
-		SetCanvasPixel(screen, cx - y, cy - x, colour);
-	}
-}
-*/ 
-
-//ellipsePlotPoints && Ellipse && ROUND
-//Revised --> http://www.programming-techniques.com/2012/01/drawing-ellipse-with-mid-point-ellipse.html
-#define ROUND(a) ((int) (a + 0.5))
-
-void ellipsePlotPoints(u8* screen, int xCenter, int yCenter, int x, int y, u32 colour){
-    SetCanvasPixel(screen, xCenter + x, yCenter + y, colour);
-    SetCanvasPixel(screen, xCenter - x, yCenter + y, colour);
-    SetCanvasPixel(screen, xCenter + x, yCenter - y, colour);
-    SetCanvasPixel(screen, xCenter - x, yCenter - y, colour);
-}
-
-void Ellipse(u8* screen, int xCenter, int yCenter, int Rx, int Ry, u32 colour){
-    int Rx2 = Rx*Rx;
-    int Ry2 = Ry*Ry;
-    int twoRx2 = 2 * Rx2;
-    int twoRy2 = 2 * Ry2;
-    int p;
-    int x = 0;
-    int y = Ry;
-    int px = 0;
-    int py = twoRx2 * y;
-
-    ellipsePlotPoints(screen, xCenter, yCenter, x, y, colour);
-
-    /* For Region 1 */
-    p = ROUND(Ry2 - (Rx2*Ry) + (0.25) * Rx2);
-    while(px < py){
-        x++;
-        px += twoRy2;
-        if(p < 0){
-            p += Ry2 + px;
-        }else{
-            y--;
-            py -= twoRx2;
-            p += Ry2 + px - py;
-        }
-        ellipsePlotPoints(screen, xCenter, yCenter, x, y, colour);
-    }
-
-    /* For Region 2*/
-    p = ROUND(Ry2 * (x + 0.5)*(x + 0.5) + Rx2 * (y - 1)*(y - 1) - Rx2 * Ry2);
-    while(y > 0){
-        y--;
-        py -= twoRx2;
-        if(p > 0){
-            p += Rx2 - py;
-        }else{
-            x++;
-            px += twoRy2;
-            p += Rx2 - py + px;
-        }
-        ellipsePlotPoints(screen, xCenter, yCenter, x, y, colour);
-    }
-
-}
-
-//pillaged --> http://www.uwgb.edu/dutchs/Geometry/HTMLCanvas/ObliqueEllipses5.HTM :: function Ellipse
-Ellipse_Rotated(u8* screen, int majaxis, int minaxis, int xctr, int yctr, int rotnrad, u32 colour){
+//pillaged --> http://www.uwgb.edu/dutchs/Geometry/HTMLCanvas/ObliqueEllipses5.HTM
+Ellipse(u8* screen, int xctr, int yctr, int majaxis, int minaxis, int rotnrad, u32 colour){
 	//Draw ellipse of arbitrary orientation
 	float t = 0; //Parametric equation variable
 	int x0 = xctr + majaxis*cos(rotnrad);// initial x-val
