e883201a6cafb64d97bd1780ad5f6c5490b91709
Kenny D. Lee <kennyd.lee@outlook.com>
2015-10-20 00:54:48 -0400

    Update imageBMP.c

--- imageBMP.c
+++ imageBMP.c
@@ -54,6 +54,45 @@ bool saveBMP(const char* path, imagebuff * image)
 	return true;
 }
 
+//found --> http://tipsandtricks.runicsoft.com/Cpp/BitmapTutorial.html#chapter4
+u8* ConvertBMPToRGBBuffer (u8* Buffer, int width, int height )
+{
+ int x, y;
+	// first make sure the parameters are valid
+	if ( ( NULL == Buffer ) || ( width == 0 ) || ( height == 0 ) )
+		return NULL;
+
+	// find the number of padding bytes
+		
+	int padding = 0;
+	int scanlinebytes = width * 3;
+	while ( ( scanlinebytes + padding ) % 4 != 0 )     // DWORD = 4 bytes
+		padding++;
+	// get the padded scanline width
+	int psw = scanlinebytes + padding;
+
+	// create new buffer
+        u8* newbuf = (u8*)malloc(width*height*3);
+//	u8* newbuf = new BYTE[width*height*3];
+	
+	// now we loop trough all bytes of the original buffer, 
+	// swap the R and B bytes and the scanlines
+	long bufpos = 0;   
+	long newpos = 0;
+	for (y = 0; y < height; y++ )
+		for (x = 0; x < 3 * width; x+=3 )
+		{
+			newpos = y * 3 * width + x;     
+			bufpos = ( height - y - 1 ) * psw + x;
+
+			newbuf[newpos] = Buffer[bufpos + 2];       
+			newbuf[newpos + 1] = Buffer[bufpos+1]; 
+			newbuf[newpos + 2] = Buffer[bufpos];     
+		}
+
+	return newbuf;
+}
+
 //revised --> Rinnegatamante :: Lua-Player-Plus :: Graphics.cpp :: Bitmap* decodeBMPfile(const char* fname)
 imagebuff * loadBMP(const char* path)
 {
@@ -74,14 +113,22 @@ imagebuff * loadBMP(const char* path)
 		svcCloseHandle(file);
 		return 0;
 	}
-	
-	result->data = (u8*)malloc(size-0x36);
-	FSFILE_Read(file, &bytesRead, 0x36, result->data, size-0x36);
-	FSFILE_Read(file, &bytesRead, 0x12, &(result->width), 4);
-	FSFILE_Read(file, &bytesRead, 0x16, &(result->height), 4);
-	FSFILE_Read(file, &bytesRead, 0x1C, &(result->depth), 2);
+	FSFILE_Read(file, &bytesRead, 0x12, &(result->width), 4); //biWidth
+	FSFILE_Read(file, &bytesRead, 0x16, &(result->height), 4); //biHeight
+	FSFILE_Read(file, &bytesRead, 0x1C, &(result->depth), 2); //biBitCount
 
-	svcCloseHandle(file);
+
+// Calculating the size of a bitmap found --> https://msdn.microsoft.com/en-us/library/ms969901.aspx
+	//biSizeImage = ((((biWidth * biBitCount) + 31) & ~31) >> 3) * biHeight:
+
+//	result->data = (u8*)malloc(size-0x36);
+	u8* tempbuf = (u8*)malloc(size-0x36);
+	FSFILE_Read(file, &bytesRead, 0x36, tempbuf, size-0x36);
+
 	FSFILE_Close(file);
+	svcCloseHandle(file);
+
+        result->data = (u8*)ConvertBMPToRGBBuffer(tempbuf, result->width, result->height);
+	free(tempbuf);
 	return result;
 }
