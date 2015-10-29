7fb59a07f1ea3663b0be47391ad7275f19135e28
Kenny D. Lee <kennyd.lee@outlook.com>
2015-10-29 08:34:58 -0400

    Update imageTGA.c

--- imageTGA.c
+++ imageTGA.c
@@ -1,32 +1,33 @@
 //large portions used --> https://github.com/citra-emu/citra/blob/master/src/video_core/utils.cpp
 
-#include 'image.h'
+#include <stdio.h>
+#include "image.h"
 
 /// Structure for the TGA texture format
-struct TGAHeader {
-    char  idlength;
-    char  colormaptype;
-    char  datatypecode; 
+typedef struct {
+    u8  idlength;
+    u8  colormaptype;
+    u8  datatypecode; 
     short int colormaporigin;
     short int colormaplength;
     short int x_origin;
     short int y_origin;
     short width;
     short height;
-    char  bitsperpixel;
-    char  imagedescriptor;
-};
+    u8  bitsperpixel;
+    u8  imagedescriptor;
+} TGAHeader;
 
-void saveTGA(const char * filename imagebuf * image) {
+void saveTGA(const char * filename, imagebuff * image) {
     TGAHeader hdr = {0, 0, 2, 0, 0, 0, 0, image->width, image->height, (image->depth << 3), 0};
     FILE* fout = fopen(filename, "wb+");
 
     fwrite(&hdr, sizeof(TGAHeader), 1, fout);
-
-    int v:
-    for (int y = 0; y < image->height; y++) {
-        for (int x = 0; x < image->width; x++) {
-            v = (result->depth * (y * width)) + (result->depth * x);
+    
+    int v,x,y;
+    for (y = 0; y < image->height; y++) {
+        for (x = 0; x < image->width; x++) {
+            v = (image->depth * (y * image->width)) + (image->depth * x);
             putc(image->data[v + 0], fout); // b
             putc(image->data[v + 1], fout); // g
             putc(image->data[v + 2], fout); // r
@@ -35,21 +36,21 @@ void saveTGA(const char * filename imagebuf * image) {
     fclose(fout);
 }
 
-imagebuf * loadTGA(const char * filename) {
+imagebuff * loadTGA(const char * filename) {
     TGAHeader hdr; // = {0, 0, 2, 0, 0, 0, 0, width, height, 24, 0};
     FILE* fout = fopen(filename, "rb+");
     fread(&hdr, sizeof(TGAHeader), 1, fout);
 
-    imagebuf * result;
+    imagebuff * result;
     result->height = hdr.height;
     result->width = hdr.width;
-    result->depth = hdr.bitsperpixel >> 3
-    result->data = (u8*)malloc(result->height * result->width * result->depth)
+    result->depth = hdr.bitsperpixel >> 3;
+    result->data = (u8*) malloc(result->height * result->width * result->depth);
     
-    int v:
-    for (int y = 0; y < result->height; y++) {
-        for (int x = 0; x < result->width; x++) {
-            v = (result->depth * (y * width)) + (result->depth * x);
+    int v,x,y;
+    for (y = 0; y < result->height; y++) {
+        for (x = 0; x < result->width; x++) {
+            v = (result->depth * (y * result->width)) + (result->depth * x);
             result->data[v + 0] = getc(fout); // b
             result->data[v + 1] = getc(fout); // g
             result->data[v + 2] = getc(fout); // r
